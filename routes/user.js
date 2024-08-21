109038ec13429c22f1dc00c9701d4dcf178509dc
anwar sadath <anwaranu633@gmail.com>
2024-08-21 21:35:23 +0530

    linkssdssggww

--- routes/user.js
+++ routes/user.js
@@ -219,7 +219,7 @@ router.get("/login", (req, res) => {
 });
 
 router.get("/end-to-end-membership", (req, res) => {
-  res.render("user/Proftfolio", { user: true });
+  res.render("user/end-to-end", { user: true });
 });
 
 router.get("/accounts", (req, res) => {
